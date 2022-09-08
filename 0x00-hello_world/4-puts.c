58efde62b5b787ab0017990ce9d218a31011c262
Mokwuah Ifeomaeze <ifyeze7272@gmail.com>
2022-09-08 16:49:03 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  *
  * * main - entry point
@@ -8,8 +9,13 @@
  * * Return: 0 number
  *
  * */
+
 int main(void)
+
 {
+
 		puts("\"Programming is like building a multilingual puzzle");
+
 			return (0);
+
 }
