55f9bae5cf45c6bed02082e524b66febaf102e9b
Mokwuah Ifeomaeze <ifyeze7272@gmail.com>
2022-09-11 15:31:37 -0700

    Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -4,10 +4,11 @@
 *You are not allowed to use any variable of type char
 *You can use the putchar function
 *Return: 0
-*/                                            
+*/                                
 
 int main(void)
 {
+
 int d;
 for (d = '0'; d <= '9'; d++)
 putchar(d);
