94e19c13e70a2eeac16de82eb80d4a3512af17ac
Mokwuah Ifeomaeze <ifyeze7272@gmail.com>
2022-09-19 16:44:14 -0700

    swap

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,1 +1,15 @@
+#include "Main.h"
+/**
+ *swap_int - function
+ *
+ *@a: pointer of parameter
+ *@b: pointer of parameter
+ */
+void swap_int(int *a, int *b)
+{
+	int temp;
 
+	temp = (*a);
+	(*a) = (*b);
+	(*b) = temp;
+}
