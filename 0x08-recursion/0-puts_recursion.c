91f28a89dd907d69d3ee7e4b13c95b895da51c90
Mokwuah Ifeomaeze <ifyeze7272@gmail.com>
2022-09-28 14:13:56 -0700

    puts recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,9 +9,10 @@ void _puts_recursion(char *s)
 {
 	if (*s)
 	{
-		_putchar(*s);
+		_putchar(char *s);
 		 _puts_recursion(s + 1);
 	}
-	 else
-		  _putchar('\n');
+
+	else
+		  _putchar(int '\n');
 }
