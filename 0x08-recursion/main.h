ad70b94a256eae63a8887523a9dc478193e6736c
Mokwuah Ifeomaeze <ifyeze7272@gmail.com>
2022-09-28 14:36:22 -0700

    main.h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,8 +1,9 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
+#ifndef main.h
+#define main.h
+
+#include<stdio.h>
 
 int _putchar(char *s);
-
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
