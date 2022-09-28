7eca6f7cf1769d2451d0f1d06f0f6cde49be2585
Mokwuah Ifeomaeze <ifyeze7272@gmail.com>
2022-09-28 16:10:23 -0700

    factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *factorial - Returns tge factorial of a given number
+ *@n: Pointer block of memory to fill
+ *
+ * Return: Factorial
+ */
+
+int factorial(int n)
+{
+	 if (n == 0)
+		 return (1);
+	 else if (n < 0)  
+		 return (-1);
+	 else
+		  return (n * factorial(n - 1));
+}
