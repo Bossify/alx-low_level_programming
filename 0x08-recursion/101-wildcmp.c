0a4c0574b3dd85a4b5f132b1542edb529d3e0856
Mokwuah Ifeomaeze <ifyeze7272@gmail.com>
2022-09-28 17:40:04 -0700

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -12,11 +12,11 @@ int substring_match(char *s1, char *s2, char *after_wldcd)
 {
 	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
-	if (s1 == '\0' && *s2 == '')
+	if (*s1 == '\0' && *s2 == '*')
 		return (substring_match(s1, s2 + 1, s2 + 1));
 	if (*s1 == '\0' && *s2 != '\0')
 		return (0);
-	if (s2 == '')
+	if (*s2 == '*')
 		return (substring_match(s1, s2 + 1, s2 + 1));
 	if (*s1 == *s2)
 		return (substring_match(s1 + 1, s2 + 1, after_wldcd));
@@ -37,8 +37,7 @@ int wildcmp(char *s1, char *s2)
 		return (1);
 	if (*s1 == *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
-
-	else if (s2 == '')
+	else if (*s2 == '*')
 		return (substring_match(s1, (s2 + 1), (s2 + 1)));
 	else
 		return (0);
